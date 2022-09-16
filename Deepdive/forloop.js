3d61bcb66476b1e6394fd999bb85dbd039922b4e
vsdittavi <vsdittakavi@gainsight.com>
2022-09-16 15:18:44 +0530

    closures

---
+++ Deepdive/forloop.js
@@ -0,0 +1,11 @@
+for(var i = 0; i < 5; i++) {
+  console.log("The value of i here is ", i);
+}
+
+let a = 19;
+
+function test() {
+
+    console.log(a); //19
+
+}
---
+++ Deepdive/typeCoercion.js
@@ -0,0 +1,10 @@
+let a = 2;
+
+let b = '3';
+
+console.log(a +b);
+
+
+console.log(true + 1);  //2  false -> 0  and   true  -> 1
+
+console.log(3-"2a");
